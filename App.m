a8d8f107e969cf8f7ca95f3a3bcdc3b160a09846
monko024 <matus@sevcek.sk>
2026-04-17 14:36:59 +0200

    temp_2

--- App.m
+++ App.m
@@ -1,20 +1,38 @@
 classdef App < handle
     properties
-        hView 
-        hModel 
-        hController 
+        hView
+        hModel
+        hController
     end
     methods
-        function obj = App()
+        function obj = App(steps)
+            if nargin < 1
+                steps = 512; % default 90 degrees
+            end
+            numPos=5;
+            % 1. Create instances
+            obj.hController = src.Controller.controller();
             obj.hModel = src.Model.model();
             obj.hView = src.View.view();
-            obj.hController = src.Controller.controller();
 
+            % 2. Link them
             obj.hController.setModel(obj.hModel);
             obj.hController.setView(obj.hView);
+<<<<<<< Updated upstream
             
             % Start the N-times loop (e.g., 18 times for 360 deg if step is 20)
             obj.hController.runScan(5); 
+=======
+
+            % 3. Execute
+            try
+                obj.hController.runScan(steps,numPos);
+                obj.hController.render();
+            catch e
+                fprintf('Error during run: %s\n', e.message);
+                delete(findall(0, 'Type', 'serialport'));
+            end
+>>>>>>> Stashed changes
         end
     end
 end
--- main.m
+++ main.m
@@ -1,2 +1,8 @@
-clear,close all
-hApp=App();
+% Clean up any leftover serial connections before starting
+delete(findall(0, 'Type', 'serialport'));
+
+% Run the app - pass number of steps:
+% 512  = 90 degrees
+% 1024 = 180 degrees
+% 2048 = 360 degrees
+hApp = App(512);
